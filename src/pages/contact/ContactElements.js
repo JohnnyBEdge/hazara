0770d305df91ea7dd4bd79d3beb3afc5c3ceb69c
John Martin <jrmartin182@gmail.com>
2020-10-19 16:12:36 -0600

    started contact form/page

--- src/pages/contact/ContactElements.js
+++ src/pages/contact/ContactElements.js
@@ -1,6 +1,69 @@
 import styled from 'styled-components';
+import ContactImg from '../../assets/contact.jpeg'
 
 export const FormContainer = styled.div`
     width:100%;
     height: 100%;
+`
+export const InfoContainer = styled.div`
+    width:100%;
+    height: 100%;
+    display: flex;
+    border: solid black 1px;
+    border-radius: 10px;
+    margin: 20px;
+`
+
+export const HeaderText = styled.h2`
+    font-size: 4rem;
+    text-align: center;
+`
+export const SubHeaderText = styled.h3`
+    font-size: 1.75rem;
+    text-align: center;
+`
+export const Us = styled.div`
+    border: solid black 1px;
+    border-radius: 10px;
+    width: 100%;
+    max-width: 600px;
+    height: 600px;
+    background-image: url(${ContactImg});
+    background-position: center;
+    color: white;
+    text-shadow: 2px 2px #000000;
+    margin: 10px;
+`
+export const IconList = styled.ul`
+    margin-top: 10px;
+    margin-left: 20px;
+    list-style: none;
+    font-size: 1.3rem
+`
+export const Icon = styled.li`
+    margin: 20px;
+`
+export const SocialM = styled.ul`
+    display: flex;
+    width: 200px;
+    justify-content: space-evenly;
+    list-style:none;
+    font-size: 2rem;
+`
+export const ListItem = styled.li`
+`
+export const Form = styled.div`
+    width: 100%;
+    align-items: center;
+    max-width: 700px;
+`
+export const InputContainer = styled.div`
+    display: flex;
+    width: 100%;
+    justify-content: space-evenly;
+    margin: 20px;
+    
+`
+export const Button = styled.button`
+
 `
--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,11 +1,85 @@
 import React from 'react';
-import {FormContainer} from './ContactElements';
+import TextField from '@material-ui/core/TextField';
+import TextareaAutosize from '@material-ui/core/TextareaAutosize';
+import {
+    FormContainer,
+    HeaderText,
+    SubHeaderText,
+    Us,
+    IconList,
+    Icon,
+    SocialM,
+    ListItem,
+    Form,
+    InfoContainer,
+    InputContainer,
+    Button
+} from './ContactElements';
+import {
+    FaPhone, 
+    FaMapPin, 
+    FaEnvelope, 
+    FaFacebook, 
+    FaTwitter, 
+    FaInstagram} from 'react-icons/fa';
+
 
 const Contact = () => {
     return (
         <>
             <FormContainer>
-                CONTACT FORM
+                <HeaderText>Contact Us</HeaderText>
+                <SubHeaderText>Questions or remarks? Send us a message!</SubHeaderText>
+
+                <InfoContainer>
+                    <Us>
+                        <IconList>
+                            <Icon>
+                                <FaPhone/> 123-456-7890
+                            </Icon>
+                            <Icon>
+                                <FaEnvelope/> email@email.com
+                            </Icon>
+                            <Icon>
+                                <FaMapPin/> 666 Elm St. Salt Lake City, Ut 84111
+                            </Icon>
+                        </IconList>
+
+                        <SocialM>
+                            <ListItem>
+                                <FaFacebook/>
+                            </ListItem>
+                            <ListItem>
+                                <FaInstagram/>
+                            </ListItem>
+                            <ListItem>
+                                <FaTwitter/>
+                            </ListItem>
+                        </SocialM>
+                    </Us>
+                <Form>
+                    <InputContainer>
+                        <TextField label="fName" defaultValue="First Name" />
+                        <TextField label="lName" defaultValue="Last Name" />
+                    </InputContainer>
+                <br/>
+                    <InputContainer>
+                        <TextField label="phone" defaultValue="Phone #" />
+                        <TextField label="email" defaultValue="Email" />
+                    </InputContainer>
+                <br/>
+                <InputContainer>
+                    <TextareaAutosize 
+                        aria-label="empty textarea" 
+                        placeholder="Type message here." 
+                        rowsMin={12}
+                        />
+                </InputContainer>
+                
+                <Button>Send Message</Button>
+
+                </Form>
+            </InfoContainer>
             </FormContainer>
         </>
     )
