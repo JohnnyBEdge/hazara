3e1656249d8fce33d40298d4987598ea688ba450
John Martin <jrmartin182@gmail.com>
2020-12-16 17:14:38 -0700

    BUILD: built form components, added formspree functionality, mobile friendly

--- src/pages/contact/ContactElements.js
+++ src/pages/contact/ContactElements.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import contact from '../../assets/images/contact3.jpg';
 import { Link } from 'react-router-dom';
+import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 
 export const PageContainer = styled.div`
     display: flex;
@@ -10,6 +11,8 @@ export const PageContainer = styled.div`
 
     @media screen and (max-width: 768px){
         flex-direction: column;
+        justify-content: center;
+        align-items: center;
     }
 `
 export const ImageContainer = styled.div`
@@ -42,6 +45,10 @@ export const RtnBtnContainer = styled.div`
     display: flex;
     justify-content: flex-end;
     width: 100%;
+    @media screen and (max-width: 768px){
+        position: relative;
+        left: 90px;
+    }
 `
 export const ReturnHomeBtn = styled(Link)`
     border-radius: 4px;
@@ -54,6 +61,7 @@ export const ReturnHomeBtn = styled(Link)`
     text-align: center;
     padding-top: 13px;
     color: black;
+    transition: 0.3s all ease;
 
     &:hover {
         background-color: #FFC559;
@@ -66,14 +74,127 @@ export const ReturnHomeBtn = styled(Link)`
 export const FormTitle = styled.h1`
     margin-left: 15%;
     margin-top: 7%;
+    width: 100%;
+    @media screen and (max-width: 768px){
+        position: relative;
+        right: 90px;
+        margin-top: 10%;
+    }
 `
 export const Form = styled.form`
-
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+    margin-top: 30px;
+    /* @media screen and (max-width: 768px){
+        width: 90%;
+    } */
+`
+export const FormSection = styled.div`
+    display: flex;
+    width: 100%;
+    justify-content: space-evenly;
+    margin-top: 10px;
+    @media screen and (max-width: 768px){
+        flex-direction: column;
+    }
+`
+export const FormGroup = styled.form`
+    display: flex;
+    flex-direction: column;
+    
+    @media screen and (max-width: 768px){
+        align-self: center;
+    }
 `
 export const FormLabel = styled.label`
-
+    margin-bottom: 0.5em;
+	color: black;
+    font-size: 15px;
+    display: block;
 `
 export const FormInput = styled.input`
+    padding: 0.5em;
+    border: none;
+	border-bottom: solid lightgray 2px;
+    width: 200px;
+	margin-bottom: 0.5em;
+    &:focus {
+        outline: none;
+        border-bottom: 2px black solid; 
+    }
+    @media screen and (max-width: 768px){
+        width: 300px;
+        margin-bottom: 1em;
+    }
 
 `
+export const Message = styled.label`
+	margin-bottom: 0.5em;
+	color: palevioletred;
+    display: none;
+`
+export const TextAreaInput = styled.textarea`
+    display: block;
+    min-height: 32px;
+    max-height: 320px;
+    line-height: 21px;
+    min-width: 400px;;
+    width: 50%;
+    margin: 0 auto;
+    box-sizing: border-box;
+    border: 1px solid rgb(217, 217, 217);
+    padding: 4px 11px;
+    text-size-adjust: 100%;
+    border: none;
+    border-bottom: 2px solid lightgray; 
+    margin-top: 5px;     
+    &:focus {
+        outline: none;
+        border-bottom: 2px solid black; 
+    }
+
+    @media screen and (max-width: 480px){
+        min-width: 350px;
+    }
+`
+export const SendMessageBtn = styled.button`
+    border-radius: 4px;
+    height: 50px;
+    width: 110px;
+    background-color: transparent;
+    border: solid black 1px;
+    margin-top: 20px;
+    text-decoration: none;
+    text-align: center;
+    color: black;
+    transition: 0.5s all ease;
+
+    &:hover {
+        background-color: #FFC559;
+        color: #1A61A3;
+        transition: 0.5s all ease;
+        width: 150px;
+    }
+    @media screen and (max-width: 768px){
+        width: 300px;
+    }
+`
+export const SocialContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 20px;
+`
+export const Icon = styled.div`
+    margin: 5px 10px;
+    cursor: pointer;
+
+    &:hover {
+        transform: scale(1.1)
+    }
+`
+
+
 
--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import logo from '../../assets/images/logo-1.jpg'
 import {
     PageContainer, ImageContainer, ImageText, FormContainer,
-    FormTitle, Form, ReturnHomeBtn,RtnBtnContainer
+    FormTitle, Form, ReturnHomeBtn,RtnBtnContainer,
+    FormGroup, FormLabel, FormInput, Message, FormSection,
+    TextAreaInput,SendMessageBtn, Icon, BtnContainer,
+    SocialContainer
 } from './ContactElements';
 import {
     FaPhone, 
@@ -11,10 +14,27 @@ import {
     FaFacebook, 
     FaTwitter, 
     FaInstagram} from 'react-icons/fa';
+import { preventDefault } from '@fullcalendar/react';
 
 
 
 const Contact = () => {
+
+    const handleForm = (e) => {
+        const form = e.target;
+        const data = new FormData(form);
+        const xhr = new XMLHttpRequest();
+        xhr.open(form.method, form.action);
+        xhr.setRequestHeader("Accept", "application/json");
+        xhr.onreadystatechange = () => {
+            if(xhr.readyState !== XMLHttpRequest.DONE) return;
+            if(xhr.status === 200) {
+                form.reset();
+            }
+        }
+        xhr.send(data);
+    }
+
     return (
         <>
             <PageContainer>
@@ -27,9 +47,73 @@ const Contact = () => {
                         <ReturnHomeBtn to='/'>Home</ReturnHomeBtn>
                     </RtnBtnContainer>
                     <FormTitle>Contact Us!</FormTitle>
-                    <Form>
+                    <Form
+                        onSubmit={(e) => handleForm(e)}
+                        action="INSERT FORMSPREE LINK HERE"
+                        method="POST">
+                        <Message>This is the validation message</Message>
+                        <FormSection>
+                            <FormGroup>
+                                <FormLabel for="fname">First Name</FormLabel>
+                                <FormInput 
+                                    id="fname" 
+                                    placeholder="Enter your first name"
+                                    type="text"
+                                    required />
+                            </FormGroup>
+                            <FormGroup>
+                                <FormLabel for="lname">Last Name</FormLabel>
+                                <FormInput 
+                                    id="lname" 
+                                    placeholder="Enter your last name"
+                                    type="text" 
+                                    required />
+                            </FormGroup>
+                        </FormSection>
+                        <FormSection>
+                            <FormGroup>
+                                <FormLabel for="email">Email</FormLabel>
+                                <FormInput 
+                                    id="email" 
+                                    type="email" 
+                                    placeholder="Enter your email"
+                                    required />
+                            </FormGroup>
+                            <FormGroup>
+                                <FormLabel for="phone">Phone Number</FormLabel>
+                                <FormInput 
+                                    id="phone" 
+                                    placeholder="Enter your phone number"
+                                    type="tel"
+                                    required />
+                            </FormGroup>
+                        </FormSection>
+                        <FormSection>
+                            <FormGroup>
+                            <FormLabel for="message">Message</FormLabel>
+                                <TextAreaInput 
+                                    type="text" 
+                                    placeholder="Leave your message here!" 
+                                    name="message"
+                                    required />
+                            </FormGroup>
+                        </FormSection>
 
+                        <SendMessageBtn >Send Message</SendMessageBtn>
                     </Form>
+                    
+                    <SocialContainer>
+                        <Icon>
+                            <FaEnvelope />
+                        </Icon>
+                        <Icon>
+                            <FaFacebook />
+                        </Icon>
+                        <Icon>
+                            <FaTwitter />
+                        </Icon>   
+                    </SocialContainer>
+
                 </FormContainer>
 
             </PageContainer>
